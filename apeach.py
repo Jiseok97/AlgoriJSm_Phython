b9851a81d451246633dcb835116a38fc448dd514
netLee <wltjr1426@naver.com>
2021-01-07 23:53:05 +0900

    또 다른 풀이 추가

--- apeach.py
+++ apeach.py
@@ -35,3 +35,40 @@ def solution(s):
         # 만들어지는 압축 문자열이 가장 짧은 것이 정답
         answer = min(answer, len(com))
     return answer
+
+
+# 또 다른 풀이
+
+def solution(s):
+    if len(s) < 3:  # 굳이 1~2개인 경우에는 어차피 1~2개가 나오므로 바로 반환
+        return len(s)
+    
+    answer = len(s)  # 후보 값
+    max_cand = int(len(s)/2)  # int를 씌우면 내림이 됨 and 쪼갤 수 있는 길이
+    for interval in range(1, max_cand +1 ):  # +1 은 max_cand까지 돌리기 위해서
+        start = interval  # 
+        res = []
+        pre_s = s[0:interval]
+        num = 1
+        while True:
+            now_s = s[start:start+interval]
+            if now_s == pre_s:
+                num += 1
+            else:
+                res.append([num.pre_s])
+                num = 1
+                pre_s = now_s
+            
+            if start > len(s):
+                break
+            start += interval
+        len_cand = 0 
+        for k in range(len(res)):
+            if res[k][0] == 1:
+                len_cand += len(res[k][1])
+            else:
+                len_cand += 1
+                len_cand += len(res[k][1])
+        answer = min(answer, len_cand)
+
+    return answer
