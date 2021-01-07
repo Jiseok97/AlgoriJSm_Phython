89c34576bacfca5ba6e49ebea4657d649bb9a26a
netLee <wltjr1426@naver.com>
2021-01-07 23:15:21 +0900

    주석 추가

--- apeach.py
+++ apeach.py
@@ -7,22 +7,31 @@
 # 출력 예시: 9 (2개 단위) -> 2ababcdcd
 
 # 프로그래머스
+# 모법 답안
 # https://programmers.co.kr/learn/courses/10336/lessons/64194
 
 def solution(s):
     answer = len(s)
     
-    for x in range(1, len(s) // 2 + 1):
+    # 1개 단위(step)부터 압축 단위를 늘려가며 확인
+    for step in range(1, len(s) // 2 + 1):
         com = ""
-        prev = s[0:x]
+        prev = s[0:step]  # 압에서부터 step만큼의 문자열 추출
         count = 1
-        for j in range(x, len(s), x):
-            if prev == s[j:j + x]:
+
+        # 단위(step) 크기 만큼 증가시키며 이전 문자열과 비교
+        for j in range(step, len(s), step):
+            # 이전 상태와 동일하다면 압축 횟수(count) 증가
+            if prev == s[j:j + step]:
                 count += 1
+            
+            # 다른 문자열이 나왔다면(더 이상 압축하지 못하는 경우라면)
             else:
                 com += str(count) + prev if count >= 2 else prev
-                prev = s[j:j + x]
+                prev = s[j:j + step]
                 count = 1
+        # 남아 있는 문자열에 대해서 처리
         com += str(count) + prev if count >= 2 else prev
+        # 만들어지는 압축 문자열이 가장 짧은 것이 정답
         answer = min(answer, len(com))
     return answer
