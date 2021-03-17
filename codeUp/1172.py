64995687d49c5546993a1ac8170fc6ef1f92ff69
netLee <wltjr1426@naver.com>
2021-03-17 18:53:05 +0900

    코드업 1172번 세 수 정렬

---
+++ codeUp/1172.py
@@ -0,0 +1,7 @@
+# 코드업 1172번 문제
+# 세수 정렬
+
+input_number = list(map(int, input().split()))
+input_number.sort()
+
+print(str(input_number[0]) + ' ' + str(input_number[1]) + ' ' + str(input_number[2]))
