ed700c0effccf239f3e7ad3e47d4ef87cbc6bac7
netLee <wltjr1426@naver.com>
2021-03-22 17:37:31 +0900

    파이썬 기초 100제_19/100

---
+++ Python/기본100제_코드업/6010_6019.py
@@ -0,0 +1,47 @@
+# 6010
+num = int(input())
+print(num)
+
+# 6011
+f = float(input())
+print(f)
+
+# 6012
+num1 = int(input())
+num2 = int(input())
+
+print(num1)
+print(num2)
+
+# 6013
+ch1 = input()
+ch2 = input()
+print(ch2)
+print(ch1)
+
+# 6014
+num = float(input())
+print(num)
+print(num)
+print(num)
+
+# 6015
+num1, num2 = input().split()
+print(num1)
+print(num2)
+
+# 6016
+ch1, ch2 = input().split()
+print(ch2, ch1)
+
+# 6017
+data = input()
+print(data, data, data)
+
+# 6018
+a, b = input().split(':')
+print(a,b, sep=':')  # sep는 분류기호로 ':' 을 두 변수 사이에 넣겠다 의미
+
+# 6019
+year, month, day = input().split('.')
+print(day, month, year, sep='-')
