433431b106868f7a852dbea2570699d60a92cf8d
netLee <wltjr1426@naver.com>
2021-03-23 21:59:35 +0900

    파이썬 기초 100제_39/100

---
+++ Python/기본100제_코드업/6030_6039.py
@@ -0,0 +1,53 @@
+# 6030
+ch = ord(input())
+print(ch)
+
+
+# 6031
+num = int(input())
+print(chr(num))
+
+
+# 6032
+num = int(input())
+print(-num)
+
+
+# 6033
+ch = ord(input())
+print(chr(ch + 1))
+
+
+# 6034
+num1, num2 = input().split()
+total = int(num1) - int(num2)
+print(total)
+
+
+# 6035
+f1, f2 = input().split()
+total = float(f1) * float(f2)
+print(total)
+
+
+# 6036
+string, count = input().split()
+print(string * int(count))
+
+
+# 6037
+count = int(input())
+string = input()
+print(string * count)
+
+
+# 6038
+a, b = input().split()
+c = int(a) ** int(b)
+print(c)
+
+
+# 6039
+f1, f2 = input().split()
+total = float(f1) ** float(f2)
+print(total)
