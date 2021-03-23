7f69725da81241a50d1f0a1bcf33cee3ced66277
netLee <wltjr1426@naver.com>
2021-03-23 22:19:19 +0900

    파이썬 기초 100제_44/100

---
+++ Python/기본100제_코드업/6040_6049.py
@@ -0,0 +1,30 @@
+# 6040
+a, b = input().split()
+print(int(a) // int(b))
+
+
+# 6041
+a, b = input().split()
+print(int(a) % int(b))
+
+
+# 6042
+data = float(input())
+print(round(data, 2))
+
+
+# 6043
+f1, f2 = input().split()
+data = float(f1) / float(f2)
+print('%.3f'%data)
+
+
+# 6044
+n1, n2 = input().split()
+data = float(n1) / float(n2)
+print(int(n1) + int(n2))
+print(int(n1) - int(n2))
+print(int(n1) * int(n2))
+print(int(n1) // int(n2))
+print(int(n1) % int(n2))
+print(round(data, 2))
