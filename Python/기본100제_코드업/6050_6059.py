0cf272f0fdadcb09f4c0579c2c441620f8f83cd8
netLee <wltjr1426@naver.com>
2021-03-24 13:19:00 +0900

    파이썬 기초 100제_59/100

---
+++ Python/기본100제_코드업/6050_6059.py
@@ -0,0 +1,48 @@
+# 6050
+n1, n2 = input().split()
+print(int(n1) <= int(n2))
+
+
+# 6051
+n1, n2 = input().split()
+print(int(n1) != int(n2))
+
+
+# 6052
+n = int(input())
+print(bool(n))
+
+
+# 6053
+n = bool(int(input()))
+print(not n)
+
+
+# 6054
+a, b = input().split()
+print(bool(int(a)) and bool(int(b)))
+
+
+# 6055
+a, b = input().split()
+print(bool(int(a)) or bool(int(b)))
+
+
+# 6056
+a, b = input().split()
+print(a != b)
+
+
+# 6057
+a, b = input().split()
+print(a == b)
+
+
+# 6058
+a, b = input().split()
+print(not(bool(int(a)) or bool(int(b))))
+
+
+# 6059
+a = int(input())
+print(~a)
