d52d3c10596c81c8c2255a3e200313bbb5873ab9
netLee <wltjr1426@naver.com>
2021-03-25 18:20:34 +0900

    파이썬 기초 100제_71/100

---
+++ Python/기본100제_코드업/6070_6079.py
@@ -0,0 +1,24 @@
+# 6070
+data = int(input())
+
+if data // 3 == 1:
+  print("spring")
+
+elif data // 3 == 2:
+  print("summer")
+
+elif data // 3 == 3:
+  print("fall")
+
+else:
+  print("winter")
+
+
+
+# 6071
+n = 1
+
+while n != 0:
+  n = int(input())
+  if n != 0:
+    print(n)
