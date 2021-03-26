370a168b6a42557f2f4b6451c49554e67835f7bc
netLee <wltjr1426@naver.com>
2021-03-26 15:46:01 +0900

    파이썬 기초 100제_79/100

--- Python/기본100제_코드업/6070_6079.py
+++ Python/기본100제_코드업/6070_6079.py
@@ -22,3 +22,81 @@ while n != 0:
   n = int(input())
   if n != 0:
     print(n)
+
+
+# 6072
+data = int(input())
+
+while data != 0:
+  print(data)
+  data -= 1
+
+
+
+# 6073
+data = int(input())
+
+while data != 0:
+  data -= 1
+  print(data)
+
+
+
+# 6074
+data = ord(input())
+compare = ord('a')
+
+while compare <= data:
+  print(chr(compare), end=' ')
+  compare += 1
+
+
+
+# 6075
+data = int(input())
+n = 0
+
+while n <= data:
+  print(n)
+  n += 1
+
+
+
+# 6076
+data = int(input())
+
+for i in range(data + 1):
+  print(i)
+
+
+
+
+# 6077
+data = int(input())
+total = 0
+
+for i in range(data + 1):
+  if i % 2 == 0:
+    total += i
+
+print(total)
+
+
+
+# 6078
+while True:
+  data = input()
+  print(data)
+  if data == 'q':
+    break
+
+
+# 6079
+data = int(input())
+total = 0
+
+for i in range(1000):
+  total += i
+  if total >= data:
+    print(i)
+    break
