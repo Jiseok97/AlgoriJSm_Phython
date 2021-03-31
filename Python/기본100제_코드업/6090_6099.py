0849c9c55b33afb311555ff3c9723ac96bd3b222
netLee <wltjr1426@naver.com>
2021-03-31 15:57:53 +0900

    파이썬 기초 100제_96/100

--- Python/기본100제_코드업/6090_6099.py
+++ Python/기본100제_코드업/6090_6099.py
@@ -91,22 +91,26 @@ for i in range(1, 20):
 
 # 6096
 # 아직 못 품
-d = [[0 for j in range(20)] for i in range(20)]
+a = [[]*19 for _ in range(19)]
+for i in range(19):
+   a[i]=list(map(int,input().split()))
 
-for i in range(20):
-  x, y = input().split()
-  for j in range(1, 20):
-    if d[j][int(y)] == 0:
-      d[j][int(y)] = 1
-    else:
-      d[j][int(y)] = 0
-      
-    if d[int(x)][j] == 0:
-      d[int(x)][j] = 1
-    else:
-      d[int(x)][j] = 0
+n = int(input())
 
-for i in range(1, 20):
-  for j in range(1, 20):
-    print(d[i][j], end =' ')
-  print()
+for i in range(n):
+    b,c=map(int,input().split())
+    
+    for j in range(19):
+        if(a[b-1][j]==1):
+            a[b-1][j]=0
+        else: a[b-1][j]=1
+    
+    for j in range(19):
+        if(a[j][c-1]==1):
+            a[j][c-1]=0
+        else: a[j][c-1]=1
+
+for i in range(19):
+    for j in range(19):
+        print(a[i][j],end=' ')
+    print()
