3e32eaddd66a6000843897bef6dc2fa51fa10f7a
netLee <wltjr1426@naver.com>
2021-05-03 13:57:03 +0900

    위에서 아래로

--- Python/Sort/위에서아래로.py
+++ Python/Sort/위에서아래로.py
@@ -4,5 +4,9 @@ for i in range(n):
   lst.append(int(input()))
 
 lst.sort(reverse = True)
+# 다른 방법
+# 파이썬 기본 정렬 라이브러리르 이용하여 정렬 수행
+# lst.sorted(lst, reverse = True)
+
 for i in lst:
   print(i, end=' ')
